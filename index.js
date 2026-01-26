4d46e19defee7c8de6c1689098deba77c1c47126
Redtag2619 <sas2619@me.com>
2026-01-26 16:29:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -55,3 +55,6 @@ const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("VoterSpheres Backend Running");
+});
