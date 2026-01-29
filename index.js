1456e1b1c6260d7b9a9518e2526423b88ec14c83
Redtag2619 <sas2619@me.com>
2026-01-28 19:18:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import pkg from "pg";
 import express from "express";
 import cors from "cors";
 import pkg from "pg";
