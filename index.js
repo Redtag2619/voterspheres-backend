3304e0be08c8d894b5291e909c2a0e9e701b243e
Redtag2619 <sas2619@me.com>
2026-02-01 10:40:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -154,8 +154,6 @@ app.get("/api/voters", async (req, res) => {
    Start Server
 ========================= */
 
-const PORT = process.env.PORT || 10000;
-
 app.listen(PORT, () => {
   console.log("Backend running on port", PORT);
 });
