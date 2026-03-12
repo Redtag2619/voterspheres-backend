a89f12341d40e3f3d1f45247085cbccd7ba7e26f
Redtag2619 <sas2619@me.com>
2026-03-11 20:59:33 -0500

    Create map.routes.js

---
+++ routes/map.routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import {
+  getStatesGeoJson,
+  getStateGeoJson,
+  runManualMapIngestion
+} from "../services/map.service.js";
+
+const router = express.Router();
+
+router.get("/geojson/states", getStatesGeoJson);
+router.get("/geojson/states/:stateName", getStateGeoJson);
+router.post("/ingest", runManualMapIngestion);
+
+export default router;
