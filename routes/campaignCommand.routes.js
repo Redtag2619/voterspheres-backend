fbd739aa9ad29fcf16029a2cdd92f464059bc90c
Redtag2619 <sas2619@me.com>
2026-03-22 07:30:26 -0500

    Update campaignCommand.routes.js

--- routes/campaignCommand.routes.js
+++ routes/campaignCommand.routes.js
@@ -1,8 +1,25 @@
 import express from "express";
-import { getCampaignCommandCenter } from "../services/campaignCommand.service.js";
+import {
+  getCampaignCommandCenter,
+  createCampaignCommandTask,
+  createCampaignCommandContact,
+  createCampaignCommandVendor,
+  createCampaignCommandDocument,
+  createCampaignCommandMailProgram,
+  createCampaignCommandMailDrop,
+  createCampaignCommandMailEvent
+} from "../services/campaignCommand.service.js";
 
 const router = express.Router();
 
 router.get("/:id/command-center", getCampaignCommandCenter);
+
+router.post("/:id/tasks", createCampaignCommandTask);
+router.post("/:id/contacts", createCampaignCommandContact);
+router.post("/:id/vendors", createCampaignCommandVendor);
+router.post("/:id/documents", createCampaignCommandDocument);
+router.post("/:id/mail-programs", createCampaignCommandMailProgram);
+router.post("/:id/mail-drops", createCampaignCommandMailDrop);
+router.post("/:id/mail-events", createCampaignCommandMailEvent);
 
 export default router;
