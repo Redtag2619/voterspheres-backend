11bb2c9ca597223ee118fe55dbec91055e966f77
Redtag2619 <sas2619@me.com>
2026-03-29 09:53:22 -0500

    Update fec.routes.js

--- routes/fec.routes.js
+++ routes/fec.routes.js
@@ -1,22 +1,42 @@
 import express from "express";
 import { requireEnterprise } from "../middleware/requirePlan.js";
-import {
-  getFundraisingLeaderboard,
-  getFundraisingSummary,
-} from "../services/fecIngestion.service.js";
+import * as fecService from "../services/fecIngestion.service.js";
 
 const router = express.Router();
+
+function resolveHandler(...names) {
+  for (const name of names) {
+    if (typeof fecService[name] === "function") {
+      return fecService[name];
+    }
+  }
+
+  return (_req, res) => {
+    return res.status(501).json({
+      error: "Route handler not implemented",
+      tried: names,
+    });
+  };
+}
 
 router.get(
   "/fundraising/leaderboard",
   requireEnterprise,
-  getFundraisingLeaderboard
+  resolveHandler(
+    "getFundraisingLeaderboard",
+    "getLeaderboard",
+    "getFecFundraisingLeaderboard"
+  )
 );
 
 router.get(
   "/fundraising/summary",
   requireEnterprise,
-  getFundraisingSummary
+  resolveHandler(
+    "getFundraisingSummary",
+    "getSummary",
+    "getFecFundraisingSummary"
+  )
 );
 
 export default router;
