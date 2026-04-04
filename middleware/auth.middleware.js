e0630fa4f0a75f469694846bdb5f37ec654c34ef
Redtag2619 <sas2619@me.com>
2026-04-04 07:45:58 -0500

    Update auth.middleware.js

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import pool from "../config/database.js";
+import pool from "../config/database.js"; 
 
 export async function requireAuth(req, res, next) {
   try {
