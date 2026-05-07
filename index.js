6c3d972862c0d6cde7e140225cdbc55496998593
Redtag2619 <sas2619@me.com>
2026-05-06 20:00:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/public", publicRoutes);
 app.use("/api/public", publicInvitesRoutes);
 app.use("/api/billing", billingRoutes);
-app.use("/api/enterprise-leads", enterpriseLeadsRoutes);
+app.use("/api/enterprise-leads", enterpriseLeadsRoutes); 
 
 app.use("/api/alerts", requireAuth, alertsRoutes);
 app.use("/api/realtime", requireAuth, realtimeRoutes);
