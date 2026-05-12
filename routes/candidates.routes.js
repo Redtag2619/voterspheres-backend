ba921e547f2382f9bec500d111a9529cdb1c4c02
Redtag2619 <sas2619@me.com>
2026-05-12 17:36:22 -0500

    Update candidates.routes.js

--- routes/candidates.routes.js
+++ routes/candidates.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import {
   fetchCandidates,
   fetchCandidateById,
-  fetchCandidateContacts,
+  fetchCandidateContacts, 
   fetchCandidateStates,
   fetchCandidateOffices,
   fetchCandidateParties,
