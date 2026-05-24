3ae4b8fe7bbbad8ac722c3b94e0d901a89dd8286
Redtag2619 <sas2619@me.com>
2026-05-24 10:19:06 -0500

    Create darkMoneyExposure.routes.js

---
+++ routes/darkMoneyExposure.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  darkMoneyExposureController,
+  darkMoneyExposureProfileController,
+} from "../controllers/darkMoneyExposure.controller.js";
+
+const router = express.Router();
+
+router.get("/", darkMoneyExposureController);
+router.get("/profile/:id", darkMoneyExposureProfileController);
+
+export default router;
