14afe81880b57060ab0396d092a004f180a0ca69
Redtag2619 <sas2619@me.com>
2026-05-27 16:08:35 -0500

    Create vendorPerformance.routes.js

---
+++ routes/vendorPerformance.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getVendorPerformance } from "../controllers/vendorPerformance.controller.js";
+
+const router = express.Router();
+
+router.get("/", getVendorPerformance);
+
+export default router;
