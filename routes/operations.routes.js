89802fa019950c2b9d1ec9a2bcee16cac07735f7
Redtag2619 <sas2619@me.com>
2026-05-30 11:02:21 -0500

    Update operations.routes.js

--- routes/operations.routes.js
+++ routes/operations.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getStateOperationsDrilldown } from "../controllers/operations.controller.js"; 
+import {
+  getStateOperationsIndex,
+  getStateOperationsDrilldown,
+} from "../controllers/operations.controller.js";
 import { requireAuth } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+router.get("/states", requireAuth, getStateOperationsIndex);
 router.get("/state/:state", requireAuth, getStateOperationsDrilldown);
 
 export default router;
