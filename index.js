121adc62fa4bcde2f158016777d89c9fb43536d4
Redtag2619 <sas2619@me.com>
2026-06-30 12:14:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import morgan from "morgan";
 
 import authRoutes from "./routes/auth.routes.js";  
 import billingRoutes from "./routes/billing.routes.js";    
-import alertsRoutes from "./routes/alerts.routes.js";
+import alertsRoutes from "./routes/alerts.routes.js"; 
 import executiveAlertEngineRoutes from "./routes/executiveAlertEngine.routes.js";  
 import operationsMapRoutes from "./routes/operationsMap.routes.js"; 
 import realtimeRoutes from "./routes/realtime.routes.js";
