c48297397f0cc9b7bc4b9c04efe795f7f14c603b
Redtag2619 <sas2619@me.com>
2026-09-07 18:10:29 -0500

    Update launchDataSeeder.routes.js

--- routes/launchDataSeeder.routes.js
+++ routes/launchDataSeeder.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { requireAuth } from "../middleware/auth.middleware.js";
-import { requirePlatformOperator } from "../middleware/authorization.middleware.js";
+import { requirePlatformOperator } from "../middleware/authorization.middleware.js"; 
 import {
   getLaunchSeederStatus,
   runLaunchDataSeeder,
