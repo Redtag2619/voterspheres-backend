3100156c8de3b7710d4b309acd723153e0e800bf
Redtag2619 <sas2619@me.com>
2026-09-07 18:11:01 -0500

    Update pollingIntelligence.routes.js

--- routes/pollingIntelligence.routes.js
+++ routes/pollingIntelligence.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { requireAuth } from "../middleware/auth.middleware.js";
-import { requirePlatformOperator } from "../middleware/authorization.middleware.js";
+import { requirePlatformOperator } from "../middleware/authorization.middleware.js"; 
 
 import {
   getPollingHealthController,
